c1909f35d512140385ce4d9757575e2d76c1c627
Justin Williams <justin.p.williams@gmail.com>
2014-07-26 15:11:39 -0700

    final

---
+++ codebook.Rmd
@@ -0,0 +1,80 @@
+Code book
+=================
+
+The objectives of this code is to present a tidy dataset base on the UCI HAR Dataset. The description for the original dataset is below. Following that, I describe how I've summarized the data.
+
+Original UCI Desctiprion: Feature Selection 
+==============
+
+The features selected for this database come from the accelerometer and gyroscope 3-axial raw signals tAcc-XYZ and tGyro-XYZ. These time domain signals (prefix 't' to denote time) were captured at a constant rate of 50 Hz. Then they were filtered using a median filter and a 3rd order low pass Butterworth filter with a corner frequency of 20 Hz to remove noise. Similarly, the acceleration signal was then separated into body and gravity acceleration signals (tBodyAcc-XYZ and tGravityAcc-XYZ) using another low pass Butterworth filter with a corner frequency of 0.3 Hz. 
+
+Subsequently, the body linear acceleration and angular velocity were derived in time to obtain Jerk signals (tBodyAccJerk-XYZ and tBodyGyroJerk-XYZ). Also the magnitude of these three-dimensional signals were calculated using the Euclidean norm (tBodyAccMag, tGravityAccMag, tBodyAccJerkMag, tBodyGyroMag, tBodyGyroJerkMag). 
+
+Finally a Fast Fourier Transform (FFT) was applied to some of these signals producing fBodyAcc-XYZ, fBodyAccJerk-XYZ, fBodyGyro-XYZ, fBodyAccJerkMag, fBodyGyroMag, fBodyGyroJerkMag. (Note the 'f' to indicate frequency domain signals). 
+
+These signals were used to estimate variables of the feature vector for each pattern:  
+'-XYZ' is used to denote 3-axial signals in the X, Y and Z directions.
+
+tBodyAcc-XYZ
+tGravityAcc-XYZ
+tBodyAccJerk-XYZ
+tBodyGyro-XYZ
+tBodyGyroJerk-XYZ
+tBodyAccMag
+tGravityAccMag
+tBodyAccJerkMag
+tBodyGyroMag
+tBodyGyroJerkMag
+fBodyAcc-XYZ
+fBodyAccJerk-XYZ
+fBodyGyro-XYZ
+fBodyAccMag
+fBodyAccJerkMag
+fBodyGyroMag
+fBodyGyroJerkMag
+
+The set of variables that were estimated from these signals are: 
+
+mean(): Mean value
+std(): Standard deviation
+mad(): Median absolute deviation 
+max(): Largest value in array
+min(): Smallest value in array
+sma(): Signal magnitude area
+energy(): Energy measure. Sum of the squares divided by the number of values. 
+iqr(): Interquartile range 
+entropy(): Signal entropy
+arCoeff(): Autorregresion coefficients with Burg order equal to 4
+correlation(): correlation coefficient between two signals
+maxInds(): index of the frequency component with largest magnitude
+meanFreq(): Weighted average of the frequency components to obtain a mean frequency
+skewness(): skewness of the frequency domain signal 
+kurtosis(): kurtosis of the frequency domain signal 
+bandsEnergy(): Energy of a frequency interval within the 64 bins of the FFT of each window.
+angle(): Angle between to vectors.
+
+Additional vectors obtained by averaging the signals in a signal window sample. These are used on the angle() variable:
+
+gravityMean
+tBodyAccMean
+tBodyAccJerkMean
+tBodyGyroMean
+tBodyGyroJerkMean
+
+The complete list of variables of each feature vector is available in 'features.txt'
+
+Summarized Data Approach
+=================
+First, we were are only interested in measurements of mean and standard deviation for all the metrics . The variable "meanandstd" identifies the hows of the metrics in the original data set that are measurements of mean and standard deviation.
+
+"features" identified the metrics collected in the original study.
+
+"trainsub", "trainx", and "trainy" are the subjects, data, and activity indexes respectively of the training set. 
+
+"test sub", "tests", and "testy" are the subjects, data, and activity indexes respectively of the test set. 
+
+"makenames" is a function that replaces the names of the features with human readable factor names.
+
+"all data" is the merged set of training and test data.
+
+"tidy" is created by taking the means of all the metrics summarized by subject and activity.
--- run_analysis.R
+++ run_analysis.R
@@ -1,8 +1,25 @@
 ## You should create one R script called run_analysis.R that does the following. 
 ## 1. Merges the training and the test sets to create one data set.
+## 2. Extracts only the measurements on the mean and standard deviation for each 
+## measurement. 
+## 3. Uses descriptive activity names to name the activities in the data set
+## 4. Appropriately labels the data set with descriptive variable names. 
+## 5. Creates a second, independent tidy data set with the average of each
+## variable for each activity and each subject. 
 
-## Read in train data. This assumes the zip file was extracted in the working
+## NOTE: This assumes the zip file was extracted in the working
 ## directory.
+
+require(plyr)
+## Load the descriptions of the measurements
+features <- read.table("UCI HAR Dataset/features.txt")
+
+## Vector with mean and std row references from the features
+meanandstd <- c(1:6, 41:46, 81:86, 121:126, 161:166, 201:202, 214:215, 227:228,
+                240:241, 253:254, 266:271, 294:296, 345:350, 373:375, 424:429, 
+                452:454, 503:504, 516:517, 529:530, 542:543)
+
+## Read in training data.
 ## Read in train subject ids
 trainsub <- read.table("UCI HAR Dataset/train/subject_train.txt", sep="")
 ## Read in train set
@@ -18,30 +35,55 @@ testx <- read.table("UCI HAR Dataset/test/X_test.txt", sep="")
 ## Read in test labels
 testy <- read.table("UCI HAR Dataset/test/Y_test.txt", sep="")
 
+## Replace each numerical reference to an activity with a descriptive activity 
+## name
+for (i in 1:6) {
+        trainy[trainy$V1 == i,] <- 
+                as.character(activityLabels[activityLabels$V1 ==i,2])
+}
+
+for (i in 1:6) {
+        testy[testy$V1 == i,] <- 
+                as.character(activityLabels[activityLabels$V1 ==i,2])
+}
+
+## Restrict train variables to Mean and STD measurements and assign names
+trainx <- trainx[,meanandstd]
+testx <- testx[,meanandstd]
+names(trainx) <- features[meandandstd,2]
+names(testx) <- features[meandandstd,2]
+
+## Give the variable names readable names using a function.
+makenames <- function(x){
+        for (i in 1:length(x)){
+                if(substr(x[i],1,1) == "t") {x[i] <- sub("t","time",x[i])}
+                if(substr(x[i],1,1) == "f") {x[i] <- sub("f","frequency",x[i])}
+        }
+        x <- sub("Acc","Acceleration",x)
+        x <- sub("Gyro","Gyroscope",x)
+        x <- sub("Mag","Magnitude",x)
+        x <- sub("mean\\(\\)","Mean",x)
+        x <- sub("meanFreq\\(\\)","MeanFrequency",x)
+        x <- sub("std\\(\\)","StandardDeviation",x)
+        x <- gsub("\\-","",x)
+        x
+}
+names(trainx) <- makenames(names(trainx))
+names(testx) <- makenames(names(testx))
+
 ## Column bind the two sets together, plus an extra variable 'set' to identify
 ## whether the data came from the train set or the test set.
-trainset <- cbind("train",trainsub,trainy,trainx)
-testset <- cbind("test", testsub,testy,testx)
+trainset <- cbind(trainsub, trainy, trainx)
+testset <- cbind(testsub, testy, testx)
 
-## Load the descriptions of the measurements
-features <- read.table("UCI HAR Dataset/features.txt")
-
-## Add subjectID, activityID, and the features descriptions as the names to the 
-## test and training data sets.
-names(trainset) <- c("set","subjectID","activityID",as.character(features[,2]))
-names(testset) <- c("set","subjectID","activityID",as.character(features[,2]))
+## Add set, subjectID, and activityID as the names to the test and training 
+## data sets.
+names(trainset)[1:2] <- c("subjectID","activity")
+names(testset)[1:2] <- c("subjectID","activity")
 
 ## Row bind the two sets together.
 alldata <- rbind(trainset,testset)
 
-
-###########################################################
-
-
-
-## 2. Extracts only the measurements on the mean and standard deviation for each 
-## measurement. 
-## 3. Uses descriptive activity names to name the activities in the data set
-## 4. Appropriately labels the data set with descriptive variable names. 
-## 5. Creates a second, independent tidy data set with the average of each
-## variable for each activity and each subject. 
+## Create the tidy data set, takign the mean of all measures by subject and 
+## activity
+tidy <- ddply(alldata, .(subjectID,activity), numcolwise(mean))
